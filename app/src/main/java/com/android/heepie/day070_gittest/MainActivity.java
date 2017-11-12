0fc72c2cac3fdee44274b98dcded81c1186ba6f3
heepie <gheepie@gmail.com>
2017-11-12 14:34:13 +0900

    no message

--- app/src/main/java/com/android/heepie/day070_gittest/MainActivity.java
+++ app/src/main/java/com/android/heepie/day070_gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.heepie.day070_gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+<<<<<<< Updated upstream
         
+=======
+        Button button;
+>>>>>>> Stashed changes
     }
 }
